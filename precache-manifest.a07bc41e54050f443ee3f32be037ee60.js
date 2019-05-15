3a5cabc7f1693842c36fb25b57a95cf063f17184
Sudipto Mondal <sudipta.just@gmail.com>
2019-05-15 14:13:30 +0600

    Updates

---
+++ precache-manifest.a07bc41e54050f443ee3f32be037ee60.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25f853604712daf7c28ee775e164b550",
+    "url": "/blog-react-app/index.html"
+  },
+  {
+    "revision": "6825f2e0e8acc1462467",
+    "url": "/blog-react-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/blog-react-app/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "6825f2e0e8acc1462467",
+    "url": "/blog-react-app/static/js/main.92c7ddef.chunk.js"
+  },
+  {
+    "revision": "f04a90e142c1312ccc5b",
+    "url": "/blog-react-app/static/js/runtime~main.3a76e1d4.js"
+  }
+]);
